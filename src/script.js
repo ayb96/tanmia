d1d6198ff9f379cf33679a6741f0984445e26d37
unknown <hussein-ayoub-96@hotmail.com>
2022-05-08 21:17:20 +0300

    some update

--- src/script.js
+++ src/script.js
@@ -1,81 +1,29 @@
 import "./style.scss";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Flip } from "gsap/Flip";
 
-gsap.registerPlugin(ScrollTrigger, Flip);
-// const header = document.querySelector(".header-main");
-
-window.addEventListener("DOMContentLoaded", (event) => {
-  console.log("dsfefadsf");
-  let layouts = ["final", "plain", "columns", "grid"],
-    container = document.querySelector(".container"),
-    curLayout = 0; // index of the current layout
-
-  function nextState() {
-    const state = Flip.getState(".letter, .for, .gsap", {
-      props: "color,backgroundColor",
-      simple: true,
-    }); // capture current state
-
-    container.classList.remove(layouts[curLayout]); // remove old class
-    curLayout = (curLayout + 1) % layouts.length; // increment (loop back to the start if at the end)
-    container.classList.add(layouts[curLayout]); // add the new class
-    var something = (function () {
-      var executed = false;
-      return function () {
-        if (!executed) {
-          executed = true;
-          console.log("run");
-        }
-      };
-    })();
-    something();
-    Flip.from(state, {
-      // animate from the previous state
-      absolute: true,
-      stagger: 0.07,
-      duration: 0.7,
-      ease: "power2.inOut",
-
-      spin: curLayout === 0, // only spin when going to the "final" layout
-      simple: true,
-      onEnter: (elements, animation) =>
-        gsap.fromTo(
-          elements,
-          { opacity: 0 },
-          { opacity: 1, delay: animation.duration() - 0.1 }
-        ),
-      onLeave: (elements) => gsap.to(elements, { opacity: 0 }),
-    });
-
-    // gsap.delayedCall(curLayout === 0 ? 3.5 : 1.5, nextState);
-    // gsap.pause();
-  }
-  nextState();
-  // gsap.delayedCall(1, nextState);
-});
+gsap.registerPlugin(ScrollTrigger);
 
 const timeline = gsap.timeline({ defaults: { duration: 1 } });
 gsap.from(".header-main", { y: "-500%", ease: "power4", duration: 2 });
 
-timeline.from(
-  ".nav-common",
-  {
-    y: "-500%",
-    delay: 0.2,
-    stagger: { each: 0.1, from: "random" },
-  },
-  "<-0.5"
-);
+// timeline.from(
+//   ".nav-common",
+//   {
+//     y: "-500%",
+//     delay: 0.2,
+//     stagger: { each: 0.1, from: "random" },
+//   },
+//   "<-0.5"
+// );
 
-timeline
-  .from(".stat-container1-com", { x: "-500%", ease: "power4", duration: 2 })
-  .from(
-    ".stat-container2",
-    { x: "500%", ease: "power4", duration: 2 },
-    "<-0.1"
-  );
+// timeline
+//   .from(".stat-container1-com", { x: "-500%", ease: "power4", duration: 2 })
+//   .from(
+//     ".stat-container2",
+//     { x: "500%", ease: "power4", duration: 2 },
+//     "<-0.1"
+//   );
 
 gsap.to(".news-container-sec22", {
   scrollTrigger: {
@@ -84,8 +32,7 @@ gsap.to(".news-container-sec22", {
     end: "bottom 100px",
     // markers: true,
     scrub: 2,
-    // duration: 2,
-    // toggleActions : play pause resume restart reset complete none
+
     toggleActions: "play pause reverse complete",
   },
   y: -40,
@@ -108,7 +55,7 @@ ScrollTrigger.create({
     timeline.restart();
   },
   // onLeaveBack: () => console.log("Leave back"),
-  markers: true,
+  // markers: true,
 });
 
 const counters = document.querySelectorAll(".counter");
@@ -137,6 +84,8 @@ function wew() {
 }
 
 const burgerButton = document.querySelector(".burger-button");
-burgerButton = document.addEventListener("burger-button", () => {
-  console.log();
+burgerButton = document.addEventListener("click", () => {
+  console.log("ghgh");
+  const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+  navbarLinks.classList.toggle("active");
 });
