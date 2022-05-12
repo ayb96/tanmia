39631f4712026b44046066b6e728fdccf8e28b3e
unknown <hussein-ayoub-96@hotmail.com>
2022-05-12 14:52:56 +0300

    some update

--- src/slider2.js
+++ src/slider2.js
@@ -1,16 +1,2 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3.5,
-  grid: {
-    rows: 2,
-  },
-  spaceBetween: 17,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+
 
